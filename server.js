959b79caf82451127151598d17a76c71f1acbaa8
Joe O'Brien <joeobrien@Joes-MacBook-Pro.local>
2016-01-24 15:21:26 +0000

    fdkjh

--- server.js
+++ server.js
@@ -9,19 +9,19 @@ app.use(bodyParser.json())
 var charities = [
   {
     "name": "Against Malaria Foundation",
-    "mondoDescription": "SHORT COURSES          LONDON        GBR".replace(/['"]+/g, ''),
+    "mondoDescription": "AGAINST MALARIA FO     ST ALBANS     GBR".replace(/['"]+/g, '').toLowerCase(),
     "merchantId": "merch_000094S7XUBOwkTnV5P1RB",
     "info": "Since its founding in 2004, the Against Malaria Foundation has distributed over 5.7 million insecticide treated nets to places in the world where malaria is most prevalent. These nets provide effective and long lasting protection to prevent someone contracting this debilitating and life threatening disease, enabling people to live happier, healthier and more productive lives. Since 2000 we have halved the number of deaths due to malaria worldwide and we have the ability to eliminate it in our lifetimes: by donating, you’ve just helped to do exactly that."
   },
   {
     "name": "War Child",
-    "mondoDescription": "WWW.WARCHILD.ORG.UK    02079169276   GBR".replace(/['"]+/g, ''),
+    "mondoDescription": "WWW.WARCHILD.ORG.UK    02079169276   GBR".replace(/['"]+/g, '').toLowerCase(),
     "merchantId": "merch_000094U3dsVDQdMJ6cxK8v",
     "info": "WarChild helps and supports children in some of the most dangerous and conflict ridden areas in the world.  Since 1993, they’ve helped millions of children affected by conflict, providing everything from school uniforms to anti-malarial treatments, and working to uphold children’s rights: the right to an education, the right to live free from violence, and ultimately, the right to a childhood.  Your donation helps support their work, enabling them to effectively help children living in some of the world’s most fragile nations, and provide these children with the childhood they deserve."
   },
   {
     "name": "Schistosomiasis Control Initiative",
-    "mondoDescription": "AGAINST MALARIA FO     ST ALBANS     GBR".replace(/['"]+/g, ''),
+    "mondoDescription": "SHORT COURSES          LONDON        GBR".replace(/['"]+/g, '').toLowerCase(),
     "merchantId": "merch_000094U2y5ktlSKRUt5DV3",
     "info": "The Schistosomiasis Control Initiative helps and support the governments of African nations treat schistosomiasis, a severely neglected tropic disease caused by parasitic worms. Since its founding in 2002, SCI have delivered well over 100 million treatments, allowing people to eliminate the parasitic disease from their bodies, and live happier, healthier and more productive lives. Whilst more than 40 million people were treated for schistosomiasis last year alone, 200 million people weren’t- together, we can reach these people, and your donation has helped to do exactly that."
   }
@@ -53,10 +53,12 @@ newTransactions = [];
 
 // webhook stuff
 app.post('/new_transaction', function(req, res){
+  newTransactions.push(JSON.stringify(transaction));
   console.log('new transaction recognised');
   var transaction = req.body.data;
   var merchant = transaction.merchant.id;
-  var name = transaction.description.replace(/['"]+/g, '');
+  var name = transaction.description.replace(/['"]+/g, '').toLowerCase();
+
 
   charities.forEach(function(charity){
     console.log('PAIR');
