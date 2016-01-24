b3f130e354204988daa1c67ce83793e3a2344ef8
Joe O'Brien <joeobrien@Joes-MacBook-Pro.local>
2016-01-24 14:13:35 +0000

    server.js parser

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var request = require('request');
 var express = require('express');
+var bodyParser = require('body-parser');
+
+
 var app = express();
+app.use(bodyParser.json())
 
 var charities = {
   "AGAINST MALARIA FOUNDATION": {
