691904f39deb399af104c3888868d6fdaa19ebfe
Joe O'Brien <joeobrien@Joes-MacBook-Pro.local>
2016-01-24 15:52:14 +0000

    cutting text down

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var charities = [
     "name": "Against Malaria Foundation",
     "mondoDescription": "AGAINST MALARIA FO".replace(/['"]+/g, '').toLowerCase(),
     "merchantId": "merch_000094S7XUBOwkTnV5P1RB",
-    "info": "Since its founding in 2004, the Against Malaria Foundation has distributed over 5.7 million insecticide treated nets to places in the world where malaria is most prevalent. These nets provide effective and long lasting protection to prevent someone contracting this debilitating and life threatening disease, enabling people to live happier, healthier and more productive lives. Since 2000 we have halved the number of deaths due to malaria worldwide and we have the ability to eliminate it in our lifetimes: by donating, you’ve just helped to do exactly that.",
+    "info": "Since its founding in 2004, the Against Malaria Foundation has distributed over 5.7 million insecticide nets around the world. These nets provide effective and long lasting protection against malaria, a debilitating and life threatening disease, enabling people to live happier, healthier and more productive lives",
     "multiple": 1.77,
     "funds": "malaria nets"
   },
@@ -19,7 +19,7 @@ var charities = [
     "name": "War Child",
     "mondoDescription": "WWW.WARCHILD.ORG.UK".replace(/['"]+/g, '').toLowerCase(),
     "merchantId": "merch_000094U3dsVDQdMJ6cxK8v",
-    "info": "WarChild helps and supports children in some of the most dangerous and conflict ridden areas in the world.  Since 1993, they’ve helped millions of children affected by conflict, providing everything from school uniforms to anti-malarial treatments, and working to uphold children’s rights: the right to an education, the right to live free from violence, and ultimately, the right to a childhood.  Your donation helps support their work, enabling them to effectively help children living in some of the world’s most fragile nations, and provide these children with the childhood they deserve.",
+    "info": "WarChild helps and supports children in some of the most dangerous and conflict ridden areas in the world.  Since 1993, they’ve helped millions of children affected by conflict, providing everything from school uniforms to anti-malarial treatments, and working to uphold children’s rights: the right to an education, the right to live free from violence, and ultimately, the right to a childhood.  ",
     "multiple": 3,
     "funds": "blankets to keep a afgani child warm in winter"
   },
@@ -27,9 +27,9 @@ var charities = [
     "name": "Schistosomiasis Control Initiative",
     "mondoDescription": "SHORT COURSES".replace(/['"]+/g, '').toLowerCase(),
     "merchantId": "merch_000094U2y5ktlSKRUt5DV3",
-    "info": "The Schistosomiasis Control Initiative helps and support the governments of African nations treat schistosomiasis, a severely neglected tropic disease caused by parasitic worms. Since its founding in 2002, SCI have delivered well over 100 million treatments, allowing people to eliminate the parasitic disease from their bodies, and live happier, healthier and more productive lives. Whilst more than 40 million people were treated for schistosomiasis last year alone, 200 million people weren’t- together, we can reach these people, and your donation has helped to do exactly that.",
-    "multiple": 37,
-    "funds": "treatmeant courses for Bill Harzia"
+    "info": "The Schistosomiasis Control Initiative helps and support the governments of African nations treat schistosomiasis, a severely neglected tropic disease caused by parasitic worms. Since its founding in 2002, SCI have delivered well over 100 million treatments, allowing people to eliminate the parasitic disease from their bodies, and live happier, healthier and more productive lives.",
+    "multiple": 0.37,
+    "funds": "treatmeant courses for Bill Harzia diseas for 1 year"
   }
 ];
 
