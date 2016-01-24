3a72707cded468ba110939ef6dcc7d7582de109c
Joe O'Brien <joeobrien@Joes-MacBook-Pro.local>
2016-01-24 12:20:02 +0000

    port stuff

--- index.js
+++ index.js
@@ -30,8 +30,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port' + app.get('port'));
 });
 
 // webhook stuff
